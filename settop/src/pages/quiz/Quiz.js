647d1df9839d0aa9d70704f7955ad207c3ef9fb7
김태연 <garong0721@gmail.com>
2023-02-10 17:50:55 +0900

    add tm end

--- settop/src/pages/quiz/Quiz.js
+++ settop/src/pages/quiz/Quiz.js
@@ -3,8 +3,7 @@ import Book from "assets/img/book_orange2.png";
 import "assets/font/font.css";
 import { useState, useEffect, useRef } from "react"; // react hook 을 사용하기 위한 import
 import { useNavigate } from "react-router-dom";
-import Timer from 'components/Timer/index.js'
-
+import Timer from "components/Timer/index.js";
 
 const myData = [
   //임시로 만들어놓은 데이터로, 추후 backend와 API통신 시 지움
@@ -77,17 +76,13 @@ const myData = [
 
 */
 const Game = () => {
+  const init = useRef(window.init);
+  const end = useRef(window.end);
   const navigate = useNavigate(); // useNavigate를 사용하기 위한 할당
   const _ = require("lodash");
   const numbers = _.range(0, 2);
   const randomanswer = []; //randomanswer: 0 또는 1이 10개 담긴 배열, 0일 경우 왼쪽이 정답, 1일 경우 오른쪽이 정답
   // 해당 코드는 컴포넌트 마운트 시 실행됨.
-
-  for (let index = 0; index < 10; index++) {
-    randomanswer.push(_.sampleSize(numbers, 1)[0]);
-  }
-
-  console.log(randomanswer);
 
   // 컴포넌트 렌더링 시 변수 초기화: useEffect hook 사용
 
@@ -103,10 +98,14 @@ const Game = () => {
 
   useEffect(() => {
     console.log("컴포넌트 마운트 됨");
-    
+
+    init.current();
     setQuestionnumber(1);
     setScore(0);
-    
+    for (let index = 0; index < 5; index++) {
+      randomanswer.push(_.sampleSize(numbers, 1)[0]);
+    }
+    console.log(randomanswer);
   }, []);
 
   useEffect(() => {
@@ -118,6 +117,7 @@ const Game = () => {
   function correct() {
     //정답을 골랐을 때
     // onIncrease();
+    console.log(randomanswer);
     setScore(score + 20);
     setQuestionnumber(questionnumber + 1);
     setGamedata(myData[questionnumber]);
@@ -125,6 +125,7 @@ const Game = () => {
     console.log("정답 눌림");
     console.log(questionnumber, "문제번호", score);
     if (questionnumber === 5) {
+      end.current();
       console.log("모든 문제 소모");
       console.log(questionnumber, "문제번호", score);
       score += 20;
@@ -141,19 +142,15 @@ const Game = () => {
 
   function incorrect() {
     //오답을 골랐을 때
+    console.log(randomanswer);
     setQuestionnumber(questionnumber + 1);
     setGamedata(myData[questionnumber]);
     if (questionnumber === 5) {
+      end.current();
       console.log("모든 문제 소모");
       navigate(`/quizresult`, { state: { score } });
     }
   }
-
-  const init = useRef(window.init);
-
-  useEffect(() => {
-    init.current();
-  },[])
 
   // const [choice, setChoice] = useState();
   // useEffect(() => {
@@ -165,9 +162,7 @@ const Game = () => {
   // if (choice === "left"){
   //   console.log(1)
   // }
-  
 
-  
   // 변한 state에 대해 렌더링
   // 이하 게임 UI 제공
   // 1. 랜덤으로 정답, 오답 제시
@@ -181,7 +176,7 @@ const Game = () => {
       <MainBlock>
         <div className="book">
           <div className="question">
-            <Timer correct={correct}/>
+            <Timer correct={correct} />
             <h3>{gamedata["question"]}</h3>
             <p>{questionnumber} / 5</p>
           </div>
@@ -196,7 +191,9 @@ const Game = () => {
                 }
               }}
             >
-              {randomanswer[questionnumber - 1] === 0 ? gamedata["answer"] : gamedata["notanswer"]}
+              {randomanswer[questionnumber - 1] === 0
+                ? gamedata["answer"]
+                : gamedata["notanswer"]}
             </button>
             <button
               className="right"
@@ -208,7 +205,9 @@ const Game = () => {
                 }
               }}
             >
-              {randomanswer[questionnumber - 1] === 0 ? gamedata["notanswer"] : gamedata["answer"]}
+              {randomanswer[questionnumber - 1] === 0
+                ? gamedata["notanswer"]
+                : gamedata["answer"]}
             </button>
           </div>
         </div>
@@ -219,65 +218,65 @@ const Game = () => {
 
 export default Game;
 
-  const MainBlock = styled.div`
-    h3 {
-      margin: 0;
-      color: #1f3995;
-      font-size: 5rem;
-      font-family: "BMEULJIRO";
-      text-align: center;
-      margin-top: 50px;
-    }
-    p {
-      margin: 0;
-      font-family: "BMEULJIRO";
-      text-align: end;
-      font-size: 3rem;
-      margin-top: 70px;
-    }
-    .book {
-      width: 1500px;
-      height: 650px;
-      background-image: url(${Book});
-      background-size: 100% 100%;
-      background-repeat: no-repeat;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-evenly;
-      align-items: center;
-    }
-    .question {
-      display: flex;
-      justify-content: space-around;
-      width: 1300px;
-      margin-top: 30px;
-      margin-left: 100px;
-    }
-    .left {
-      background-color: #cecece;
-      border: transparent;
-      border-radius: 10px;
-      width: 550px;
-      height: 350px;
-      font-size: 3rem;
-      font-family: "BMEULJIRO";
-    }
-    .right {
-      background-color: transparent;
-      border-radius: 10px;
-      width: 550px;
-      height: 350px;
-      font-size: 3rem;
-      font-family: "BMEULJIRO";
-    }
-    .btn {
-      margin-left: 100px;
-      width: 1300px;
-      display: flex;
-      justify-content: space-evenly;
-      margin-bottom: 30px;
-    }
+const MainBlock = styled.div`
+  h3 {
+    margin: 0;
+    color: #1f3995;
+    font-size: 5rem;
+    font-family: "BMEULJIRO";
+    text-align: center;
+    margin-top: 50px;
+  }
+  p {
+    margin: 0;
+    font-family: "BMEULJIRO";
+    text-align: end;
+    font-size: 3rem;
+    margin-top: 70px;
+  }
+  .book {
+    width: 1500px;
+    height: 650px;
+    background-image: url(${Book});
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
     display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
     align-items: center;
-    justify-content: center;
-  `;
+  }
+  .question {
+    display: flex;
+    justify-content: space-around;
+    width: 1300px;
+    margin-top: 30px;
+    margin-left: 100px;
+  }
+  .left {
+    background-color: #cecece;
+    border: transparent;
+    border-radius: 10px;
+    width: 550px;
+    height: 350px;
+    font-size: 3rem;
+    font-family: "BMEULJIRO";
+  }
+  .right {
+    background-color: transparent;
+    border-radius: 10px;
+    width: 550px;
+    height: 350px;
+    font-size: 3rem;
+    font-family: "BMEULJIRO";
+  }
+  .btn {
+    margin-left: 100px;
+    width: 1300px;
+    display: flex;
+    justify-content: space-evenly;
+    margin-bottom: 30px;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
