6c1e180e72cebab73eaf8ac9b6bf4c2029e9d156
kunwoo park <scv74502@naver.com>
2023-02-16 01:02:19 +0900

    sql db init query

--- back-end/src/main/resources/pjt1_v1.sql
+++ back-end/src/main/resources/pjt1_v1.sql
@@ -2,7 +2,11 @@
 --
 -- Host: 127.0.0.1    Database: springboot
 -- ------------------------------------------------------
--- Server version	10.6.11-MariaDB
+-- Server version	10.6.11-
+-- reset database schema
+drop database if exists springboot;
+create database springboot;
+use springboot;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
